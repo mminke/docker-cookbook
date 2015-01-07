ed6935544bda440abc5e27ef34fed2b0f1228e8e
Morten Minke <m.minke@kataru.nl>
2015-01-07 11:34:15 +0100

    Use CATALINA_HOME env variable to start tomcat instead of hardcoded path

--- zikru/0.0.1-SNAPSHOT/scripts/start-zikru.sh
+++ zikru/0.0.1-SNAPSHOT/scripts/start-zikru.sh
@@ -9,6 +9,7 @@ echo --------------------------------------------------------------------
 echo ZIKRU_HOME=$ZIKRU_HOME
 echo MONGODB_SERVER_URL=$MONGODB_SERVER_URL
 echo ZIKRU_BACKEND_WEBSERVICE_URL=$ZIKRU_BACKEND_WEBSERVICE_URL
+echo CATALINA_HOME=$CATALINA_HOME
 echo ====================================================================
 echo Using zikru-config.properties:
 echo --------------------------------------------------------------------
@@ -16,4 +17,4 @@ cat $ZIKRU_HOME/conf/zikru-config.properties
 echo ====================================================================
 
 # Start tomcat
-/opt/apache-tomcat-7.0.50/bin/catalina.sh run
+$CATALINA_HOME/bin/catalina.sh run
